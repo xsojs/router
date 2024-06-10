b4fcd4047e2fe2f659e371d56caccc205f347880
Eduardo Velasques <eduveks@gmail.com>
2024-06-10 11:36:37 -0300

    Navigate supports anchor.

--- src/Navigate.js
+++ src/Navigate.js
@@ -3,14 +3,24 @@ import { navigateTo } from "./main";
 
 function Navigate(props) {
     const {to, top, onClick} = props;
-    delete props.to;
-    delete props.top;
-    delete props.onClick;
+    const elementProps = {...props};
+    delete elementProps.to;
+    delete elementProps.top;
+    delete elementProps.onClick;
     this.view(() => [
         {
             a: {
                 onClick: () => {
-                    navigateTo(to);
+                    let link = to;
+                    let anchor = null;
+                    if (to.indexOf('#') >= 0) {
+                        link = to.substring(0, to.indexOf('#'));
+                        anchor = to.substring(to.indexOf('#') + 1);
+                    }
+                    navigateTo(link);
+                    if (anchor) {
+                        window.setTimeout(() => location.hash = "#"+ anchor, 0);
+                    }
                     if (top !== false) {
                         window.scrollTo(0, 0);
                     }
@@ -18,7 +28,7 @@ function Navigate(props) {
                         onClick();
                     }
                 },
-                ...props
+                ...elementProps
             }
         }
     ]);
